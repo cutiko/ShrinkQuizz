503ef632859243fd7cc22785e09f0beb9303b150
cutiko <cutiko@gmail.com>
2016-08-02 08:42:38 -0400

    ADD: results are working

--- app/src/main/java/cl/cutiko/shrinkquizz/views/match/MatchFragment.java
+++ app/src/main/java/cl/cutiko/shrinkquizz/views/match/MatchFragment.java
@@ -1,17 +1,24 @@
 package cl.cutiko.shrinkquizz.views.match;
 
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import cl.cutiko.shrinkquizz.R;
+import cl.cutiko.shrinkquizz.data.LuckyResult;
+import cl.cutiko.shrinkquizz.data.MatchResult;
 
 public class MatchFragment extends Fragment {
 
@@ -45,8 +52,23 @@ public class MatchFragment extends Fragment {
         sendBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (quarterRg.getCheckedRadioButtonId() != -1) {
-
+                int selectedId = quarterRg.getCheckedRadioButtonId();
+                if (selectedId != -1) {
+                    RadioButton selected = (RadioButton) quarterRg.findViewById(selectedId);
+                    Context context = getContext();
+                    String result = new MatchResult(userName.getText().toString(), loverName.getText().toString(), selected.getText().toString(), context).details();
+                    Log.d("RESULT", result);
+                    AlertDialog.Builder dialog = new AlertDialog.Builder(context);
+                    dialog.setMessage(result);
+                    dialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                        @Override
+                        public void onDismiss(DialogInterface dialogInterface) {
+                            userName.setText("");
+                            loverName.setText("");
+                            quarterRg.clearCheck();
+                        }
+                    });
+                    dialog.show();
                 } else {
                     Toast.makeText(getContext(), "Seleccione nacimiento", Toast.LENGTH_SHORT).show();
                 }
