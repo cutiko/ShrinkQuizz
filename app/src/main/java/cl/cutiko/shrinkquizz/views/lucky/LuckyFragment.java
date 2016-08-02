737e7988e669aaa1c69d396fdcf4fed9cc152e3d
cutiko <cutiko@gmail.com>
2016-08-02 06:10:26 -0400

    ADD: results for lucky fragment

--- app/src/main/java/cl/cutiko/shrinkquizz/views/lucky/LuckyFragment.java
+++ app/src/main/java/cl/cutiko/shrinkquizz/views/lucky/LuckyFragment.java
@@ -1,15 +1,21 @@
 package cl.cutiko.shrinkquizz.views.lucky;
 
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 
 import cl.cutiko.shrinkquizz.R;
+import cl.cutiko.shrinkquizz.data.CoolResult;
+import cl.cutiko.shrinkquizz.data.LuckyResult;
 
 
 public class LuckyFragment extends Fragment {
@@ -45,11 +51,28 @@ public class LuckyFragment extends Fragment {
             }
         });
 
+        Button sendBtn = (Button) main.findViewById(R.id.sendBtn);
+        setSendBtn(sendBtn);
 
         return main;
     }
 
-    private void displayResults(){
-
+    private void setSendBtn(Button sendBtn){
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Context context = getContext();
+                LuckyResult result = new LuckyResult(happy.isChecked());
+                AlertDialog.Builder dialog = new AlertDialog.Builder(context);
+                dialog.setMessage(result.detaisl());
+                dialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                    @Override
+                    public void onDismiss(DialogInterface dialogInterface) {
+                        happy.setChecked(false);
+                    }
+                });
+                dialog.show();
+            }
+        });
     }
 }
