414b2a849f4ea3ef96040822e7a607f47b3c9cb0
cutiko <cutiko@gmail.com>
2016-08-02 07:50:17 -0400

    ADD: layout and basic ui functionality

--- app/src/main/java/cl/cutiko/shrinkquizz/views/match/MatchFragment.java
+++ app/src/main/java/cl/cutiko/shrinkquizz/views/match/MatchFragment.java
@@ -6,10 +6,17 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import cl.cutiko.shrinkquizz.R;
 
 public class MatchFragment extends Fragment {
+
+    private EditText userName, loverName;
+    private RadioGroup quarterRg;
 
     public MatchFragment() {
         // Required empty public constructor
@@ -18,16 +25,33 @@ public class MatchFragment extends Fragment {
         return new MatchFragment();
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_match, container, false);
+        View main = inflater.inflate(R.layout.fragment_match, container, false);
+
+        userName = (EditText) main.findViewById(R.id.userName);
+        loverName = (EditText) main.findViewById(R.id.loverName);
+        quarterRg = (RadioGroup) main.findViewById(R.id.quarterRg);
+
+        Button sendBtn = (Button) main.findViewById(R.id.sendBtn);
+        setSendBtn(sendBtn);
+
+        return main;
+    }
+
+    private void setSendBtn(Button sendBtn){
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (quarterRg.getCheckedRadioButtonId() != -1) {
+
+                } else {
+                    Toast.makeText(getContext(), "Seleccione nacimiento", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
 }
